98d1ba6aa4763dc93fd314fde761ce97fced53fa
Xinli Wang <XW@XinlideMBP.fios-router.home>
2016-02-29 03:07:14 -0500

    plots

---
+++ plot1.R
@@ -0,0 +1,8 @@
+setwd("/Users/XW/Downloads")
+data <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), sep = ";", header = F)
+names <- read.table("household_power_consumption.txt", nrows = 1, sep = ";")
+colnames(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+Global_Active_Power <- as.numeric(data$Global_active_power)
+png("plot1.png",width = 480, height = 480)
+hist(Global_Active_Power,xlab="Global Active Power (kilowaltts)",main="Global Active Power",xlim=c(0,6),ylim=c(0,1200),col = "red")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,11 @@
+setwd("/Users/XW/Desktop/ExData_Plotting1")
+data <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), sep = ";", header = F)
+names <- read.table("household_power_consumption.txt", nrows = 1, sep = ";")
+colnames(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime <- strptime(data$DateTime, "%d/%m/%Y %H:%M:%S")
+png("plot2.png", width = 480, height = 480)
+plot(data$DateTime, data$Global_active_power, type="l", xlab="",ylab = "Global Active Power (kilowatts)")
+dev.off()
